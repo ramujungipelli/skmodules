342d025eb38534a87e9dd4d4056ef6ffe1967e90
Kaushik S <koushik501s@gmail.com>
2023-07-26 20:33:44 +0530

    Update main.tf

--- modules/vms/main.tf
+++ modules/vms/main.tf
@@ -1,3 +1,7 @@
+locals {
+  first_public_key  = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC5VCYgMWQYyIxe7hJJelZKgWohVghv0988dlPvxwqbluWx5DWqBj3lGqmKKQs6o2srs/YQKpFLmRlqZV+R8uRnZauz51zDkSc2cHsiGIrDj/jTHMQmX65vv0DNhRPwmmUfeoCbsZ4RWgKg+QR2nFlnWHdj/yOGwCYOdz8gYczSaMbdohG0J1ED41bMxSt2w2CMqeIYaDUMa8/iAfyVv8HW9FV/DsuBHbvqZRNknqqANeh45LePfBHvLIrk7YuLgrFu0wXgoj5Nmm45uVEm2Lf9WhmAUsgXycbEN/k5CutkXkJ+SDdodPoCT6ekiCMDZXhMSuyxRd1RXKLu1GFyo+GZLdTNRu9Lhgua5Asbm6TB4wGqiLA+Up9Jgi39Afl2DGMywzWoZdiyL3XRlxFYSOIHDrFAYWPx/XML+1JQePq8K3k6zt0l5T87XG8VZJ2aWdgtdmTvgrUb4PfgUIJbxL35t0bYmvFoI4Hff1GyA5tchmdxPaC1Uc7xeOtH6MCrpuk= koush@hgdlaptop01"
+}
+
 # data "azurerm_resource_group" "resource_group" {
 #   name     = var.resource_group_name
 #   location = var.resource_group_location
@@ -34,10 +38,12 @@ resource "azurerm_linux_virtual_machine" "ubuntu_linux_server" { # reference nam
   ]
   disable_password_authentication = true
 
-#  admin_ssh_key {
- #   username   = var.username
-   # public_key = file("C:/Users/koush/.ssh/id_rsa.pub")
-  #}
+ admin_ssh_key {
+   username   = var.username
+ #public_key = file("C:/Users/koush/.ssh/id_rsa.pub")
+    public_key = local.first_public_key
+
+}
 
   os_disk {
     caching              = "ReadWrite"
