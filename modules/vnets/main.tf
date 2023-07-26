5a2a8efba1afd82c6b4fb95482ca80bb591e8c17
Kaushik S <koushik501s@gmail.com>
2023-07-26 20:40:27 +0530

    Update main.tf

--- modules/vnets/main.tf
+++ modules/vnets/main.tf
@@ -36,7 +36,9 @@ resource "azurerm_subnet" "subnet_name" {
   resource_group_name = var.resource_group_name
   virtual_network_name = var.virtual_network_name
   address_prefixes     = ["172.16.10.0/24"]
-
+depends_on = [
+    azurerm_virtual_network.mainhubvnet
+  ]
 }
 output "subnet_id" {
     value = azurerm_subnet.subnet_name.id
